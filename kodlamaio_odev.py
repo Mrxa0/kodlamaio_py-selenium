fd9da94bc2d0f03a4db2c759be6971a5ce589ca6
Mrxa0 <miirzaturan@gmail.com>
2023-03-09 15:43:47 +0300

    Create kodlamaio_odev.py

---
+++ kodlamaio_odev.py
@@ -0,0 +1,39 @@
+--Soru 1: Pyhton'da veri tipleri--
+# =============================================================================
+# Python'da veri tipleri çoğu zaman otomatik ayarlanır ancak yine de elle de atayabiliriz.
+# int : içerisinde tam sayılar tutulur.sık kullanılır.Sayısal bir ifade tutulmak istendiğinde genellikle int kullanılır.
+# 
+# float: Ondalıklı sayıları tutmak için kullanılır.Eğer sayısal işlemlerimizde daha hassas sonuçlar elde etmek istiyorsak veyahut küsüratlı sayılarla işlemler yapmak istiyorsak float kullanılır.
+# 
+# str: Metinsel ifadeleri tutar.Int gibi sık kullanılır.
+# 
+# bool: Bir döngünün doğrulunu veya koşulun sağlanıp sağlanmadığını kontrol edebilirz. (Evet/hayır,doğru/yanlış)
+# 
+# type(): Veri tipleri otomatik atanıyor olabilir ama biz yinede hangi verinin hangi türde olduğunu görmek istersek type ile görebiliriz.
+# =============================================================================
+
+--Soru 2:Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.--
+
+# =============================================================================
+# Kullanıcı adlarımız== str
+# 
+# Kurs ilerlemelerimiz== int
+# =============================================================================
+
+--Soru 3:Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.--
+# =============================================================================
+# user = "mrxa"
+# password = "0000"
+# 
+# print("Kullanici adinizi girin: ")
+# x = str(input());
+# 
+# print("Sifrenizi girin: ")
+# y = str(input());
+# 
+# if user == x and password == y:
+#     print("--Giris yapılmıstır--")
+#     
+# elif user!= x or password !=y:
+#     print("**Kullanici adi veya sifre hatali**")
+# =============================================================================
